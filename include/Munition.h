808369e564a7b6390960385feac68ad01db98d23
Gregory ADAM <gregory.adam@etu.unistra.fr>
2022-04-29 10:29:48 +0200

    Entity draw generalisation

--- include/Munition.h
+++ include/Munition.h
@@ -5,23 +5,15 @@
 #include <SDL.h>
 #include <vector>
 #include "Map.h"
+#include "Entity.h"
 
-class Munition
+class Munition: public Entity
 {
 private:
-	std::unique_ptr<Point<int>> velocity;
-	std::shared_ptr<std::vector<Line>> shape;
-	int position;
-	int tunnel_position;
-
 
 public:
-	Munition(std::unique_ptr<Point<int>> velocity, std::shared_ptr<std::vector<Line>> shape, int position): 
-		velocity{ std::move(velocity) }, shape{ shape }, position{ position }
+	Munition(int speed, std::shared_ptr<std::vector<Line>> shape, int position, int tunnel_position):
+		Entity{ speed, shape, position, tunnel_position }
 	{};
-	Munition(int position) :
-		position{ position }
-	{};
-	void draw(Map& m) const;
 };
 #endif
