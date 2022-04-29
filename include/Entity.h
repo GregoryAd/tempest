83c4aa9bd67fae37b2b7f5db7bac5afe4c146308
Gregory ADAM <gregory.adam@etu.unistra.fr>
2022-04-29 10:04:51 +0200

    Entity class

---
+++ include/Entity.h
@@ -0,0 +1,32 @@
+#ifndef ENTITY
+#define ENTITY
+
+#include <iostream>
+#include <SDL.h>
+#include <vector>
+
+#include "Line"
+#include "Map.h"
+
+class Entity
+{
+private:
+	int speed;
+	std::shared_ptr<std::vector<Line>> shape;
+	int position;
+	int tunnel_position;
+
+public:
+	Entity(int speed, std::shared_ptr<std::vector<Line>> shape, int position, int tunnel_position):
+		speed{ speed }, shape{ shape }, position{ position }, tunnel_position{ tunnel_position }
+	{
+
+	}
+
+	const int& getSpeed() const;
+	const int& getPosition() const;
+	const int& getTunnel_position() const;
+
+};
+
+#endif 
