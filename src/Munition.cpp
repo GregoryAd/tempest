c83c93a588a4f8071bc11ecedfb3a540ae04bda3
Gregory ADAM <gregory.adam@etu.unistra.fr>
2022-04-29 09:58:17 +0200

    Munition size correction bug

--- src/Munition.cpp
+++ src/Munition.cpp
@@ -15,26 +15,31 @@ void Munition::draw(Map& m) const {
 
 	Line directionLine{std::make_unique<Point<int>>(in), std::make_unique<Point<int>>(out) };
 
+	int size = lineOut.getSize() / 10;
+	int mid = size / 2;
+
 	Point<int> direction = *directionLine.lineToVector();
-	Point<double> fdir = *direction.normalize();
-
-	Point<int> normalDirection{direction.getY(), direction.getX()};
-	Point<double> fnorm = *normalDirection.normalize();
-
-	int size = lineOut.getSize()/10;
+	Point<double> fdir = *direction.normalize() * size;
+	fdir = Point<double>{ fdir.getX(), fdir.getY() };
 
 
-	fnorm = fnorm * size;
-	fdir = fdir * size;
+
+	//Point<int> normalDirection{direction.getY(), direction.getX()};
+	Point<double> fnorm{ fdir.getY(), fdir.getX() };
 
 
-	Point<int> p1{ static_cast<int>((fdir.getX() + fnorm.getX()) * 0.5) , static_cast<int>((fdir.getY() + fnorm.getY()) * 0)};
+
+	//fnorm = fnorm * size;
+	//fdir = fdir * size;
+
+
+	Point<int> p1{ static_cast<int>((fdir.getX() + fnorm.getX()) * 0.5) + mid , static_cast<int>((fdir.getY() + fnorm.getY()) * 0)};
 	p1 = p1 + *lineOut.center();
-	Point<int> p2{ static_cast<int>((fdir.getX() + fnorm.getX()) * 0) , static_cast<int>((fdir.getY() + fnorm.getY()) * 0.5) };
+	Point<int> p2{ static_cast<int>((fdir.getX() + fnorm.getX()) * 0) + mid, static_cast<int>((fdir.getY() + fnorm.getY()) * 0.5) };
 	p2 = p2 + *lineOut.center();
-	Point<int> p3{ static_cast<int>((fdir.getX() + fnorm.getX()) * 0.5) , static_cast<int>((fdir.getY() + fnorm.getY()) * 1) };
+	Point<int> p3{ static_cast<int>((fdir.getX() + fnorm.getX()) * 0.5) + mid, static_cast<int>((fdir.getY() + fnorm.getY()) * 1) };
 	p3 = p3 + *lineOut.center();
-	Point<int> p4{ (static_cast<int>(fdir.getX() + fnorm.getX()) * 1) , static_cast<int>((fdir.getY() + fnorm.getY()) * 0.5) };
+	Point<int> p4{ static_cast<int>((fdir.getX() + fnorm.getX()) * 1) + mid, static_cast<int>((fdir.getY() + fnorm.getY()) * 0.5) };
 	p4 = p4 + *lineOut.center();
 
 	Line l1{std::make_unique<Point<int>>(p1), std::make_unique<Point<int>>(p2)};
