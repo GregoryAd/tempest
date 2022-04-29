21770a844af76399270f751120a1831f04cc55c5
estherlenotre <riversouls.l@gmail.com>
2022-04-29 15:43:40 +0200

    enemymanager

--- include/EnemyManager.h
+++ include/EnemyManager.h
@@ -6,7 +6,13 @@
 class EnemyManager {
 private :
 	std::vector<std::unique_ptr<Enemy>> enemies{};
+	int count; // compteur pour spawn d'ennemi
+	int rate; // fréquence de spawn
+	double probability; // probabilité de spawn
 public :
+	EnemyManager(int rate, double probability) (enemies{}, count{ 0 }, rate{ rate }, probability{ probability })
+	 {};
+	void init();
 	void update(const Map& m);
 };
 
--- src/EnemyManager.cpp
+++ src/EnemyManager.cpp
@@ -1,17 +1,52 @@
 #include "EnemyManager.h"
+#include "Map.h"
 
 // Gestion des ennemis
-void Enemy::update(const Map& m) {
-
-	this->draw(m);
+void EnemyManager::update(const Map& m) {
+	count++;
 	for (int i = 0; i < enemies.size(); i++) {
 		// faire avancer les ennemis
-		enemies[i]->move();
-		enemies[i]->draw(m);
+		if (enemies[i]->getStatus() == true) {
+			enemies[i]->move();
+			enemies[i]->draw(m);
+		}
 
 		// si l'ennemi est mort, le retirer de la liste
-		if (enemies[i].getStatus == 0) {
-			enemies[i].erase(enemies.begin() + i);
+		if (enemies[i]->getStatus() == false) {
+			enemies.erase(enemies.begin() + i);
 		}
 	}
+}
+
+// génération d'ennemis
+void EnemyManager::init(const Map& m){
+	std::vector<Line> shape2 = {
+	Line{ 0, 0, 50, 5 },
+	Line{ 50, 5, 100, 0 },
+	Line{ 100, 0, 87, 5 },
+	Line{ 87, 5, 95, 10 },
+	Line{ 95, 10, 50, 5 },
+	Line{ 50, 5, 3, 10 },
+	Line{ 3, 10, 10, 5 },
+	Line{ 10, 5, 0, 0 },
+	};
+
+	// proba aléatoire
+	std::random_device rd; 
+	std::mt19937 gen(rd());
+	std::uniform_real_distribution<> dis(0.0, 1.0);
+
+	// position sur la map aléatoire
+	std::random_device rd2;
+	std::mt19937 gen2(rd2());
+	std::uniform_int_distribution<> dis2(1, m.getSize());
+
+	// génération d'un ennemi
+	if (dis(gen) > probability) && (count == rate)) {
+		count = 0;
+		Enemy e{ -5, std::make_shared<std::vector<Line>>(shape2), dis2(gen2), 100, 0.90, 100 };
+		e.setStatus(true);
+		enemies.push_back(std::make_unique<Enemy>(e));
+	}
+	
 }
